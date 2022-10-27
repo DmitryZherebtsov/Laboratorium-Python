cf4255f3e9c3d7f56fafdc9d62e51f3c32164140
Dmity <zherebtsovdima31@gmail.com>
2022-10-27 11:13:01 +0200

    Warunki

--- Lab_2/Warunki.py
+++ Lab_2/Warunki.py
@@ -1,1 +1,28 @@
-print("Warunki")
+print("""Jaką operację chcesz wykonać?
+1) dodawanie
+2) odejmowanie
+3) mnożenie
+4) dzielenie
+5) potęgowanie""")
+i = int(input("Wpisz numer operacji: "))
+
+a = int(input("Podaj argument 1: "))
+b = int(input("Podaj argument 2: "))
+
+if i == 1:
+    print("Wynik: ", a + b)
+elif i == 2:
+    print("Wynik: ", a - b)
+elif i == 3:
+    print("Wynik: ", a * b)
+elif i == 4:
+    if a == 0:
+        print("Błąd: Spróbuj ponownie")
+        exit()
+    print("Wynik: ", a / b)
+elif i == 5:
+    print("Wynik: ", a % b)
+else:
+    print("Spróbuj ponownie")
+    exit()
+print(" ")
