c68951c9e0c99915b40a041fe949c2705abcfba8
Dmity <zherebtsovdima31@gmail.com>
2022-11-04 20:04:16 +0100

    Zadanie

---
+++ Lab_4/1.py
@@ -0,0 +1,10 @@
+lista_zakupow = ['chelb', 'masło', 'jajka', 'jabłka']
+
+print('Pierwszy i drugi element z listy: ', lista_zakupow[0], lista_zakupow[1])
+
+lista_zakupow[2] = 'mleko'
+print('Trzeci element listy: ', lista_zakupow[2])
+
+a = len(lista_zakupow) - 1
+print('Przedostatni i ostatni element listy: ', lista_zakupow[a - 1], lista_zakupow[a])
+#print('Przedostatni i ostatni element listy: ', lista_zakupow[-2], lista_zakupow[-1])
