f959deb6bcc9dac1c381e781ea8069c86bc0178c
Dmity <zherebtsovdima31@gmail.com>
2022-11-06 23:54:24 +0100

    Zadania

--- Lab_3/9.py
+++ Lab_3/9.py
@@ -1,10 +1,11 @@
-
+#Pirwsza wersja
 n = int(input())
-i = 0
-j = 0
-gwiazdki = ""
+for i in range(n+1):
+    print("" * (n-i)+"*"*i)
 
-for i in range(n):
-    gwiazdki = gwiazdki + "*"
-    i+=1
-    print(gwiazdki)
+#Druga wersja
+n = int(input())
+for i in range(n+1):
+    print(" " * (n-i)+"* "*i)
+
+
