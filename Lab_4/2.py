14798203cca8496114fc434138853ed109bb5385
DmitryZherebtsov <79690647+DmitryZherebtsov@users.noreply.github.com>
2022-11-10 14:31:12 +0100

    Zadania

---
+++ Lab_4/2.py
@@ -0,0 +1,21 @@
+zestaw_1=[]
+import random
+y = int(input("Podaj liczbe elemtnow listy: "))
+for i in range(y):
+    x = random.randint(1,10)
+    zestaw_1.append(x)
+print(zestaw_1)
+
+
+j = int(input("Podaj liczbe elemtnow listy: "))
+zestaw_2=[random.randint(5, 15) for i in range(j)]
+print(zestaw_2)
+
+
+f = int(input("Podaj liczbe: "))
+if f in zestaw_1:
+    print("Liczba z zestawu 1 występuje")
+elif f in zestaw_2:
+    print("Liczba z zestawu 2 występuje")
+else:
+    print("Nie ma takiej liczby w obu zestawach")
---
+++ Lab_4/4.py
@@ -0,0 +1,15 @@
+import random
+# punkty = []
+
+# for i in range(15):
+#     p = random.uniform(0, 50)
+#     p = round(p,2)
+#     # punkty.append(round(p))
+#     punkty.append(p)
+
+punkty = [round(random.uniform(0, 50),2) for a in range(15)]
+print(punkty)
+print("Minimalne: ", min(punkty))
+print("Maxymalnie: ", max(punkty))
+
+# for punkty in range(1, 15):
